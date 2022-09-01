2d9f57ace9e31d2a18f7b1f1892c023ea86e0218
piratekev <93568025+piratekev@users.noreply.github.com>
2022-09-01 12:07:55 -0700

    Update mempool.go

--- lib/mempool.go
+++ lib/mempool.go
@@ -17,8 +17,11 @@ import (
 	"strings"
 	"sync/atomic"
 	"time"
+	
+	"github.com/dgraph-io/badger/v3"
 
 	"github.com/btcsuite/btcd/btcec"
+	"github.com/deso-protocol/go-deadlock"
 	"github.com/golang/glog"
 	"github.com/pkg/errors"
 )
